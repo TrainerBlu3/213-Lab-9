9389464a0327a8f935267ed6bf4153efa75a0741
Matthew Tindogan <77546248+TrainerBlu3@users.noreply.github.com>
2025-11-05 16:26:49 -0800

    Implement User class with constructor and describe method

---
+++ user.php
@@ -0,0 +1,22 @@
+<?php
+
+class User{
+
+  public $name;
+  public $email;
+
+  public function __construct($name, $email){
+    $this->name = $name;
+    $this->email = $email;
+  }
+
+  public function describe(){
+    return "{$this->name} ({$this->email})";
+  }
+
+  $u1 = new User("Alice", "alice@example.com");
+  echo $u1->describe();
+
+}
+
+?>
